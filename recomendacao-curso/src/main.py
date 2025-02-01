61baa028b5ece545e9b6cc02f874e7324f4ba4bf
Ângelo Hank <hankangelo6@gmail.com>
2025-02-01 13:37:36 -0300

    criando populacao

--- recomendacao-curso/src/main.py
+++ recomendacao-curso/src/main.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+import random
 usuarios_dataset = pd.read_csv('../resources/datasets/users.csv')
 classificacoes_dataset = pd.read_csv('../resources/datasets/ratings.csv')
 
@@ -9,3 +9,16 @@ courses = classificacoes_dataset['Item'].unique().tolist()
 TAMANHO_POPULACAO = 10
 QT_GERACOES = 50
 TAXA_MUTACAO = 0.1 #TODO esses valores podem ser alterados para serem recebidos dinamicamente na execucao
+
+def gerar_populacao():
+    return [(random.choice(students), random.choice(courses)) for _ in range(TAMANHO_POPULACAO)]
+
+def exec_process():
+    populacao = gerar_populacao()
+
+    #TODO implementar
+    best_recommendation = []
+    return best_recommendation
+
+melhor_recomendacao = exec_process()
+print(f"melhor recomendação: Estudante: {melhor_recomendacao[0]} -> curso: {melhor_recomendacao[1]}")
