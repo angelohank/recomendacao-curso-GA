cdee888e8f8238ce01b17225e1cf9677bdf64405
Ângelo Hank <hankangelo6@gmail.com>
2025-02-01 13:46:49 -0300

    implementando funcao de mutacao, fitness e crossover

--- recomendacao-curso/src/main.py
+++ recomendacao-curso/src/main.py
@@ -10,14 +10,44 @@ TAMANHO_POPULACAO = 10
 QT_GERACOES = 50
 TAXA_MUTACAO = 0.1 #TODO esses valores podem ser alterados para serem recebidos dinamicamente na execucao
 
+def fitness(recomendacao):
+    student, course = recomendacao
+
+    classificacao_estudante = classificacoes_dataset[(classificacoes_dataset['UserID'] == student) & (classificacoes_dataset['Item'] == course)]
+    if not classificacao_estudante.empty:
+        score = classificacao_estudante['Rating'].mean().mean()
+        return score
+    else:
+        return 0
+
 def gerar_populacao():
     return [(random.choice(students), random.choice(courses)) for _ in range(TAMANHO_POPULACAO)]
+
+def seleciona_individuos(populacao):
+    populacao_ordenada = sorted(populacao, key=fitness, reverse=True)
+    return populacao_ordenada[:2]
+
+def crossover(individuo1, individuo2):
+    estudante = individuo1[0]
+    curso = individuo2[1]
+    return (estudante, curso)
+
+
+def mutate(recomendacao):
+    if random.random() < TAXA_MUTACAO:
+        return (recomendacao[0], random.choice(courses))
+    return recomendacao
+
 
 def exec_process():
     populacao = gerar_populacao()
 
-    #TODO implementar
-    best_recommendation = []
+    for geracao in range(QT_GERACOES):
+        individuos = seleciona_individuos(populacao)
+        filhos = [mutate(crossover(*individuos)) for _ in range(TAMANHO_POPULACAO)]
+        population = individuos + filhos
+
+    best_recommendation = max(populacao, key=fitness)
     return best_recommendation
 
 melhor_recomendacao = exec_process()
