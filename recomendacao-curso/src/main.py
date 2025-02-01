1ce5f5639bbedd46da244d4a08e9375b8e5b5f23
Ângelo Hank <hankangelo6@gmail.com>
2025-02-01 13:30:46 -0300

    carregando dados dos datasets com pandas e definindo variaveis do algoritmo

--- recomendacao-curso/src/main.py
+++ recomendacao-curso/src/main.py
@@ -1,2 +1,11 @@
-if __name__ == '__main__':
-    print("hello world")
+import pandas as pd
+
+usuarios_dataset = pd.read_csv('../resources/datasets/users.csv')
+classificacoes_dataset = pd.read_csv('../resources/datasets/ratings.csv')
+
+students = usuarios_dataset['UserID'].unique().tolist()
+courses = classificacoes_dataset['Item'].unique().tolist()
+
+TAMANHO_POPULACAO = 10
+QT_GERACOES = 50
+TAXA_MUTACAO = 0.1 #TODO esses valores podem ser alterados para serem recebidos dinamicamente na execucao
